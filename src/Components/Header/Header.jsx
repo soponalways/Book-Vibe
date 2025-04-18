467b829af1e4c4f280259a8c9a9e92af4a549ce0
soponalways <soponalways@gmail.com>
2025-04-18 23:14:57 +0600

    Create Folder Strcture

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Hello from Header </h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h2>Hello from Root Component</h2>
+        </div>
+    );
+};
+
+export default Root;
