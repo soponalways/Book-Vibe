176bff7d347bccf4d74e80b427452ed8d122a730
soponalways <soponalways@gmail.com>
2025-04-18 22:54:58 +0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button className='btn btn-accent btn-outline'>HI</button>
+    </>
+  )
+}
+
+export default App
